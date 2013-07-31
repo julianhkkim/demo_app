333498f75c6140949053ff80f6b15dec80f60727
Julian Hyung-Kyoon Kim <jhyungkyoonkim@groupon.com>
2013-07-31 15:29:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '5d39d413071a552052a4c5d1ce48ec11aa65e3446ace00cd77f5c929fc22fa9525a983f8fd13e0e67ca8531ef767d37c9714740ce85ddddec1e5f135d4338d12'
